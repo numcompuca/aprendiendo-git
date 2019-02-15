71edd38a23381cbf83ccc944be86ef4e2ea0802a
Juanan7676 <juan4999@gmail.com>
2019-02-15 17:54:55 +0100

    Juan Antonio; inicializando repositorio de prueba

---
+++ prueba.py
@@ -0,0 +1,10 @@
+import os
+
+# Programa principal del repositorio
+# Añade tu nombre con un print(), guarda los cambios haciendo
+# git commit -am "<descripción del cambio>"
+# y luego sube tu commit usando git push -u origin
+
+print("Juan Antonio")
+
+os.system("pause")
